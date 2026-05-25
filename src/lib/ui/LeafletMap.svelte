d98a2f4007f3a972385f76d48406b303c898b4f1
masterichigo92 <trooting@gmail.com>
2026-05-26 00:09:46 +0100

    leaflet markers not loading properly when deployed - FIXED

--- src/lib/ui/LeafletMap.svelte
+++ src/lib/ui/LeafletMap.svelte
@@ -19,6 +19,14 @@
   onMount(async () => {
     const leaflet = await import("leaflet");
     L = leaflet.default;
+   // --- FIX FOR BROKEN PRODUCTION MARKERS ---
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    });
+
     baseLayers = {
       Terrain: leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 17,
